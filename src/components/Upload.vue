188dbda8a270fa64ff65f21c6d12c9aa887cc1e4
ernestchu <b073040018@nsysu.edu.tw>
2022-02-04 15:53:57 +0800

    update

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -3,17 +3,30 @@
     <h1>上傳{{ upload.title }}</h1>
     <input id="file" type="file" :multiple="!!upload.multiple" @change="filesSelected">
     <div id="dropbox" class="dropbox" :style="{ '--color': accentStyle.color, '--background-color': accentStyle.backgroundColor }">
-      <template v-if="files">
-      <ul>
+      <ul v-if="files">
         <li v-for="(file, index) in files" :key="index">{{ file.name }}</li>
       </ul>
-      </template>
-      <template v-else>
-      <svg id="icon" class="icon" xmlns="http://www.w3.org/2000/svg" width="50" height="43" viewBox="0 0 50 43" :style="{ '--color': accentStyle.color }">
+      <svg
+        v-show="!files"
+        id="icon"
+        class="icon"
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="43"
+        viewBox="0 0 50 43"
+        :style="{ '--color': accentStyle.color }"
+        @mouseover="mouseover"
+        @mouseout="mouseout"
+      >
         <path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z" />
       </svg>
-      </template>
-      <label for="file" :style="{ '--color': accentStyle.color }">
+      <label
+        id="label"
+        for="file"
+        :style="{ '--color': accentStyle.color }"
+        @mouseover="mouseover"
+        @mouseout="mouseout"
+      >
         選取檔案<span>或<span id="drag-label">拖曳至此</span></span>
       </label>
     </div>
@@ -45,14 +58,14 @@ export default {
       dropbox.addEventListener(event, () => {
         dropbox.classList.add('is-dragover')
         document.getElementById('drag-label').classList.add('drag-label-active')
-        if (!this.files) document.getElementById('icon').classList.add('icon-active')
+        document.getElementById('icon').classList.add('icon-active')
       })
     })
     'dragleave dragend drop'.split(' ').forEach(event => {
       dropbox.addEventListener(event, () => {
         dropbox.classList.remove('is-dragover')
         document.getElementById('drag-label').classList.remove('drag-label-active')
-        if (!this.files) document.getElementById('icon').classList.remove('icon-active')
+        document.getElementById('icon').classList.remove('icon-active')
       })
     })
     dropbox.addEventListener('drop', e => {
@@ -72,6 +85,14 @@ export default {
     }
   },
   methods: {
+    mouseover () {
+      document.getElementById('label').classList.add('active')
+      document.getElementById('icon').classList.add('icon-active')
+    },
+    mouseout () {
+      document.getElementById('label').classList.remove('active')
+      document.getElementById('icon').classList.remove('icon-active')
+    },
     filesSelected (e) {
       this.files = e.target.files
     }
@@ -120,9 +141,12 @@ export default {
 .icon {
   width: 100%;
   height: 80px;
-  display: block;
-  margin-bottom: 40px;
+  position: fixed;
   transition: all .3s;
+  top: 50%;
+  left: 50%;
+  margin-top: -40px; /* Negative half of height. */
+  margin-left: -50%; /* Negative half of width. */
 }
 .icon-active {
   transform: scale(1.1);
@@ -132,8 +156,12 @@ label {
   cursor: pointer;
   letter-spacing: 1px;
   transition: all .3s;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 15%;
 }
-label:hover {
+label.active {
   letter-spacing: 0;
   font-size: 23px;
   font-weight: bold;
